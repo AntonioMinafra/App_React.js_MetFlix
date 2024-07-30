8b212baa0506a9bad1877b659fcb2fe55732c329
Antonio <antoniominafra.dev@gmail.com>
2024-07-30 17:45:34 +0200

    added movie api and set base code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+// 9701bf54
+
+const API_URL = 'http://www.omdbapi.com?apikey=9701bf54';
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1> Hello , jsm</h1>
-    </div>
-  );
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+        console.log(data.Search);
+    }
+    useEffect( () => {
+        searchMovies('thor')
+    }, []);
+
+    return (
+        <h1>App</h1>
+    )
 }
 
 export default App;
