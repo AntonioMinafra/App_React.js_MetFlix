7950244cb8b03c2e6578851e1ddf04687e9b141e
Antonio <antoniominafra.dev@gmail.com>
2024-07-30 18:53:55 +0200

    added input search

--- src/App.js
+++ src/App.js
@@ -6,18 +6,10 @@ import MovieCard from "./MovieCard";
 
 const API_URL = 'http://www.omdbapi.com?apikey=9701bf54';
 
-const movie1 =
-{
-    "Title": "Thor",
-    "Year": "2011",
-    "imdbID": "tt0800369",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg"
-}
-
 const App = () => {
 
     const [movies, setMovies] = useState( [] );
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
@@ -34,13 +26,13 @@ const App = () => {
             <div className="search">
                 <input
                     placeholder="Search for movies"
-                    value="Superman"
-                    onChange={ () => {}}
+                    value={searchTerm}
+                    onChange={ (e) => setSearchTerm(e.target.value)}
                 />
                 <img
                 src={SearchIcon}
                 alt="search"
-                onClick={ () => {}}
+                onClick={ () => searchMovies(searchTerm)}
                 />
             </div>
 
